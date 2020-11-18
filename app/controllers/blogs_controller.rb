f23a817a5f33d27a263e1cb92e7420942c387006
wa-r <nyuuuuun14@gmail.com>
2020-11-18 08:43:09 +0000

    [Start] 3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,15 @@
 class BlogsController < ApplicationController
-  def index
-  end
-
   def show
   end
 
+  def index
+  end
+
   def new
+    @blog = Blog.new
+  end
+
+  def create
   end
 
   def edit
