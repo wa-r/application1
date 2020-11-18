fc47e0cbec5e923c110826e5018bc42ed712d1a1
wa-r <nyuuuuun14@gmail.com>
2020-11-18 10:29:56 +0000

    [Finish] 3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,9 +12,9 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(blog_params)
+    blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
